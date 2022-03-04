74b88b5e29839009081f488ab92fe079c07ff349
Emiliano Rodriguez <emirod1955@gmail.com>
2022-03-04 01:25:54 -0800

    react reveal agregado

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,5 +1,6 @@
 //Import React
 import React from "react";
+import Fade from 'react-reveal/Fade';
 
 //Import styles
 import './Download.css';
@@ -9,8 +10,7 @@ function Download(){
     return(          
             <div className="download">
 
-                
-                <div className="MacOSBox">
+                <Fade><div className="MacOSBox">
                     <div className="DivsDownload"> 
                         <div>
                             <img id="AppleImage" src="https://camo.githubusercontent.com/6c1b3c0aee1fbf3885dc250f490e5fb2bb1ba1e5b4a7afa24a5547126945bd4a/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f313136313531372f73637265656e73686f74732f373839363037362f6170706c652d6c6f676f2d616e696d6174696f6e2e676966" alt="Rocket IMG"/>   
@@ -20,14 +20,14 @@ function Download(){
                             
                             <h1>Download for <b id="MacOSLetter">MacOS</b></h1>
 
-                            <a href="https://drive.google.com/file/d/1FOFxHdBVBZ6d_BeXz9hDCLAu1abCDGh_/view?usp=sharing"><input id="MacOSButton" type="button" value="Download now"></input></a>
+                            <a href="https://drive.google.com/file/d/1FOFxHdBVBZ6d_BeXz9hDCLAu1abCDGh_/view?usp=sharing" download><input id="MacOSButton" type="button" value="Download now"></input></a>
 
                         </div>
                     </div>
-                </div>
+                </div></Fade>
+                
 
-
-                <div className="WindowsBox">
+                <Fade><div className="WindowsBox">
                     <div className="DivsDownload"> 
                         <div className="WindowsImageBox">
                             <img id="WindowsImage" src="https://itblogsogeti.files.wordpress.com/2015/08/windows_10.png" alt="Rocket IMG"/>   
@@ -45,10 +45,10 @@ function Download(){
                             </div>
                         </div>
                     </div>
-                </div>
+                </div></Fade>
+                
 
-
-                <div className="LinuxBox">
+                <Fade><div className="LinuxBox">
                     <div className="DivsDownload"> 
                         
                         <div className="LinuxImageBox">
@@ -61,7 +61,7 @@ function Download(){
         
                         </div>
                     </div>
-                </div>
+                </div></Fade>
 
             </div>
             
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 //Import React
 import React from "react";
 import { Link } from 'react-router-dom'
+import Fade from 'react-reveal/Fade';
 
 //Import styles
 import './Home.css';
@@ -13,18 +14,19 @@ const Home = () =>{
            <img src="https://i.ibb.co/64rmHkH/image-3.png" alt="rocket"/>
            </div>
 
-            <div className="Divs"> 
-                <div className="TextDiv">
-                    <h2 className="subtitle">THE GAME</h2>
-                    <br />
-                    <p>
-                        We have developed the solution to the poor accessibility of scientific data. An intuitive and interesting video game/simulator, able to encourage young people to investigate the wonders of the Universe and space missions.
-                    <br /><br />
-                        We present: Moon Lander, a video game with real physics where the user must control a rocket and accomplish the mission.
-                    <br /><br />
-                        The game has a protagonist and a good amount of data, emphasizing the next mission of Artemis.
-                    <br />
-                    </p>
+           <Fade>
+               <div className="Divs"> 
+                    <div className="TextDiv">
+                        <h2 className="subtitle">THE GAME</h2>
+                        <br />
+                        <p>
+                            We have developed the solution to the poor accessibility of scientific data. An intuitive and interesting video game/simulator, able to encourage young people to investigate the wonders of the Universe and space missions.
+                            <br /><br />
+                            We present: Moon Lander, a video game with real physics where the user must control a rocket and accomplish the mission.
+                            <br /><br />
+                            The game has a protagonist and a good amount of data, emphasizing the next mission of Artemis.
+                            <br />
+                        </p>
                     <Link to="/download">
                         <button className="button">See download options</button>
                     </Link>
@@ -34,29 +36,33 @@ const Home = () =>{
                     <img id="RocketImage" alt="Rocket IMG" src="https://i.ibb.co/xF89w2D/image-1.png"/>   
                 </div>
             </div>
+            </Fade>
+            
+            
+<Fade>
+    <div className="Divs"> 
 
-            <div className="Divs"> 
+<div className="ImgBackground">
+        <img id="RocketImage" alt="Rocket IMG" src="https://i.ibb.co/xGTTjqr/image.png"/>   
+    </div>
 
-            <div className="ImgBackground">
-                    <img id="RocketImage" alt="Rocket IMG" src="https://i.ibb.co/xGTTjqr/image.png"/>   
-                </div>
+    <div className="TextDiv">
+        <h2 className="subtitle">SPACE AGENCY DATA</h2>
+        <br />
+        <p>
+        We used 3D models provided by NASA as a reference for some models and to place in the video game, so that it looks more beautiful.
+        <br /><br />
+        We are inspired by Artemis' mission to inspire girls to be part of scientific research too. The main character is a woman since we make reference to the first woman to land on the Moon.
+        <br /><br />
+        We used data from the Starship to make the physics realistic.
+        <br />
+        </p>
+    </div>
+</div>
+</Fade>
 
-                <div className="TextDiv">
-                    <h2 className="subtitle">SPACE AGENCY DATA</h2>
-                    <br />
-                    <p>
-                    We used 3D models provided by NASA as a reference for some models and to place in the video game, so that it looks more beautiful.
-                    <br /><br />
-                    We are inspired by Artemis' mission to inspire girls to be part of scientific research too. The main character is a woman since we make reference to the first woman to land on the Moon.
-                    <br /><br />
-                    We used data from the Starship to make the physics realistic.
-                    <br />
-                    </p>
-                </div>
-            </div>
-
-
-                <div className="Divs"> 
+<Fade>
+    <div className="Divs"> 
                 <div className="TextDiv">
                     <h2 className="subtitle">HOW TO PLAY</h2>
                     <br />
@@ -78,8 +84,10 @@ const Home = () =>{
                             </div>
                 </div>
             </div>
+            </Fade>
                     
-            <div className="TimeLineBox">
+            <Fade>
+                <div className="TimeLineBox">
                     <h1 className="subtitle">THE SIMULATOR MISSIONS UBICATED IN TIME</h1>
                     <div className="DivsDownload"> 
                         <div>
@@ -87,6 +95,8 @@ const Home = () =>{
                         </div>
                     </div>
                 </div>
+                </Fade>
+
         </div>
     );
 }
